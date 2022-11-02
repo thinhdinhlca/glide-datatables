aed0e1bf11a2fd3e6e62e3d3d373f11ad4fb1038
Darren Murphy <darren@victoriajd.com>
2022-11-02 14:44:54 +0800

    Revert optional exports

--- function.js
+++ function.js
@@ -1,8 +1,6 @@
-window.function = function (table, tableID, allow_export) {
+window.function = function (table, tableID) {
   if (table.value === undefined) return undefined;
   if (tableID.value === undefined) return undefined;
-  var export = allow_export.value ? allow_export.value : false;
-  var export_js = '';
 
   const BASE_OPTIONS = `
     <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
@@ -26,28 +24,20 @@ window.function = function (table, tableID, allow_export) {
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     ${BASE_OPTIONS}
-  `;
-
-  if (export === true) {
-    html += EXPORT_OPTIONS;
-    export_js = `
-      {
+    ${EXPORT_OPTIONS}
+  </head>
+  <body>
+   ${table.value}
+  </body>
+  </html>
+  <script type="text/javascript" class="init">
+    $(document).ready(function () {
+      $('#${tableID.value}').DataTable({
         dom: 'Bfrtip',
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
         ]
-    }`;
-  }
-
-  html += `
-    </head>
-    <body>
-   ${table.value}
-    </body>
-    </html>
-    <script type="text/javascript" class="init">
-    $(document).ready(function () {
-      $('#${tableID.value}').DataTable(${export_js});
+      });
     });
   </script>
   `;
