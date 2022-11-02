9a9df803eceda25e3c6b1303bb4366a6cdf9db36
Darren Murphy <darren@victoriajd.com>
2022-11-02 15:27:52 +0800

    Reorder DOM options

--- function.js
+++ function.js
@@ -44,7 +44,7 @@ window.function = function (table, tableID) {
     $(document).ready(function () {
       $('#${tableID.value}').DataTable({
         colReorder: true,
-        dom: 'BlRfrtip',
+        dom: 'lBRfrtip',
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print', 'colvis'
         ]
