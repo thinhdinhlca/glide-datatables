e2bf000ff646a8e2916a22dff4a8d7c374b63eed
Darren Murphy <darren@victoriajd.com>
2022-11-03 17:31:37 +0800

    Fix default when no button selected

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 window.function = function (table, tableID, buttons) {
   if (table.value === undefined) return undefined;
   if (tableID.value === undefined) return undefined;
-  var buttonSpec = buttons.value ? buttons.value : 'pdf';
+  var buttonSpec = buttons.value ? buttons.value : `''`;
 
   const BASE_OPTIONS = `
     <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
