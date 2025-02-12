4602159b046881ab450159265b21777331a0e627
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2025-02-12 22:09:31 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,16 +1,17 @@
-window.function = function (table, tableID, buttons, colReorder, domOptions, defaultPageLength) {
+window.function = function (table, tableID, buttons, colReorder, domOptions, defaultPageLength, enabledDarkMode) {
   if (table.value === undefined) return undefined;
   if (tableID.value === undefined) return undefined;
   var buttonSpec = buttons.value ? buttons.value : `''`;
   var col_ordering = colReorder.value ? colReorder.value : false;
   var dom = domOptions.value ? domOptions.value : 'BRfrtlip';
   var pageLength = defaultPageLength.value ? parseInt(defaultPageLength.value, 10) : 10; // Default to 10 if not provided.
+  var darkModeClass = enabledDarkMode ? 'dark' : '';
 
   const BASE_OPTIONS = `
     <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.6.3.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-    `;
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
+  `;
 
   const EXPORT_OPTIONS = `
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.3.2/css/buttons.dataTables.min.css">
@@ -31,15 +32,15 @@ window.function = function (table, tableID, buttons, colReorder, domOptions, def
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/fixedheader/3.3.1/js/dataTables.fixedHeader.min.js"></script>
   `;
 
-  const COLREORDER_OPTIONS = col_ordering === true ? 
+  const COLREORDER_OPTIONS = col_ordering === true ?
     `
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/colreorder/1.6.1/css/colReorder.dataTables.min.css">
     <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/colreorder/1.6.1/js/dataTables.colReorder.min.js"></script>
     ` : '';
-  
+
   var html = `
   <!DOCTYPE html>
-  <html>
+  <html class="${darkModeClass}">
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     ${BASE_OPTIONS}
@@ -87,5 +88,5 @@ table {
 
   var enc = encodeURIComponent(html);
   var uri = `data:text/html;charset=utf-8,${enc}`
-  return uri; 
+  return uri;
 }
